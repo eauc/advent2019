1e2f772516651f5dbd933fbc053dc7eabd48d4ae
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2019-12-03 11:37:05 +0100

    Day 2

--- src/main/advent2019/core.cljs
+++ src/main/advent2019/core.cljs
@@ -19,10 +19,94 @@
 
 (comment
 
-  (reduce + (map #(-> % (js/parseInt 10) module-fuel-requirement) input-lines))
+  (reduce + (map module-fuel-requirement modules-masses))
   ;; 5115845
   ;; 3412496
 
-  (def input-lines
-    (clojure.string/split-lines
-      (fs/readFileSync "./input.txt"))))
+  (def modules-masses
+    (map
+      #(js/parseInt % 10)
+      (clojure.string/split-lines
+        (fs/readFileSync "./modules_masses.txt"))))
+
+  )
+
+
+(defn assoc-output
+  [output n value]
+  (concat (take n output)
+          (list value)
+          (drop (inc n) output)))
+
+
+(defmulti run-opcode
+  (fn [program output]
+    (first program))
+  :default 99)
+
+
+(defmethod run-opcode 99
+  [program output]
+  [(list) output])
+
+(defmethod run-opcode 1
+  [[_ op-a op-b op-result & prog-rest] output]
+  (let [result (+ (nth output op-a)
+                  (nth output op-b))]
+    [prog-rest (assoc-output output op-result result)]))
+
+(defmethod run-opcode 2
+  [[_ op-a op-b op-result & prog-rest] output]
+  (let [result (* (nth output op-a)
+                  (nth output op-b))]
+    [prog-rest (assoc-output output op-result result)]))
+
+
+(defn run-program
+  ([program output]
+   (if (= 0 (count program))
+     output
+     (let [[prog-rest new-output] (run-opcode program output)]
+       (run-program (take-last (count prog-rest) new-output) new-output))))
+  ([program]
+   (run-program program program)))
+
+
+(defn call-program
+  [program noun verb]
+  (-> program
+      (assoc-output 1 noun)
+      (assoc-output 2 verb)
+      run-program
+      first))
+
+
+(comment
+
+  (run-opcode [99] [1 0])
+  (run-opcode [1 0 0 0 99] [1 0 0 0 99])
+  (run-opcode [2 3 0 3 99] [2 3 0 3 99])
+
+  (run-program [99])
+  (run-program [1 0 0 0 99])
+  (run-program [2 3 0 3 99])
+  (run-program [2 4 4 5 99 0])
+  (run-program [1 1 1 4 99 5 6 0 99])
+
+  (def gravity-program
+    (map
+      #(js/parseInt % 10)
+      (clojure.string/split
+        (fs/readFileSync "./gravity_program.txt")
+        #",")))
+
+  (call-program gravity-program 12 2)
+  ;; 4570637
+
+  (filter
+    #(= 19690720 (nth % 2))
+    (for [noun (range 30 60)
+          verb (range 99)]
+      [noun verb (call-program gravity-program noun verb)]))
+
+  )
--- src/test/advent2019/core_test.cljs
+++ src/test/advent2019/core_test.cljs
@@ -8,3 +8,10 @@
   (is (= 2 (core/module-fuel-requirement 14)))
   (is (= 966 (core/module-fuel-requirement 1969)))
   (is (= 50346 (core/module-fuel-requirement 100756))))
+
+(deftest run-program-test
+  (is (= [99] (core/run-program [99])))
+  (is (= [2 0 0 0 99] (core/run-program [1 0 0 0 99])))
+  (is (= [2 3 0 6 99] (core/run-program [2 3 0 3 99])))
+  (is (= [2 4 4 5 99 9801] (core/run-program [2 4 4 5 99 0])))
+  (is (= [30 1 1 4 2 5 6 0 99] (core/run-program [1 1 1 4 99 5 6 0 99]))))
