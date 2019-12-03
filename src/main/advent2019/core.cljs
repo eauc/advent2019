cff669876dfd6c0c0fc1e3a4947157e5dcbba715
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2019-12-03 12:53:13 +0100

    Day 3

--- src/main/advent2019/core.cljs
+++ src/main/advent2019/core.cljs
@@ -1,10 +1,6 @@
 (ns advent2019.core
-  (:require ["fs" :as fs]))
-
-
-(defn main [& cli-args]
-  (prn "hello world"))
-
+  (:require ["fs" :as fs]
+            [clojure.set]))
 
 (defn module-fuel-requirement
   [module-mass]
@@ -110,3 +106,113 @@
       [noun verb (call-program gravity-program noun verb)]))
 
   )
+
+
+(defn decoded-wire
+  [wire]
+  (map #(vector (subs % 0 1)
+                (js/parseInt (subs % 1) 10)) wire))
+
+
+(defmulti move->positions
+  (fn [[direction distance] from]
+    direction))
+
+(defmethod move->positions "R"
+  [[direction distance] [from-x from-y]]
+  (map
+    #(vector (+ from-x (inc %)) from-y)
+    (range distance)))
+
+(defmethod move->positions "L"
+  [[direction distance] [from-x from-y]]
+  (map
+    #(vector (- from-x (inc %)) from-y)
+    (range distance)))
+
+(defmethod move->positions "D"
+  [[direction distance] [from-x from-y]]
+  (map
+    #(vector from-x (- from-y (inc %)))
+    (range distance)))
+
+(defmethod move->positions "U"
+  [[direction distance] [from-x from-y]]
+  (map
+    #(vector from-x (+ from-y (inc %)))
+    (range distance)))
+
+
+(defn wire->positions
+  [wire]
+  (loop [[move & moves-rest] (decoded-wire wire)
+         positions [[0 0]]]
+    (if (nil? move)
+      (rest positions)
+      (let [new-positions (move->positions move (last positions))]
+        (recur moves-rest (concat positions new-positions))))))
+
+
+(defn wires-intersections
+  [wire-positions]
+  (apply
+    clojure.set/intersection
+    (map set wire-positions)))
+
+(defn distance-to-point
+  [wire-positions point]
+  (inc (count (take-while #(not= point %) wire-positions))))
+
+
+(defn wires->closest-intersection
+  [wires]
+  (let [wires-positions (map wire->positions wires)
+        intersections (wires-intersections wires-positions)
+        int-distances (map
+                        (fn [positions]
+                          (map #(distance-to-point positions %) intersections))
+                        wires-positions)]
+    (apply min
+           (apply map #(+ (js/Math.abs %1)
+                          (js/Math.abs %2))
+                  int-distances))))
+
+
+(comment
+
+  (move->positions ["R" 8] [10 5])
+  (move->positions ["D" 8] [10 5])
+  (move->positions ["L" 8] [10 5])
+  (move->positions ["U" 8] [10 5])
+
+  (wire->positions ["R5" "D3" "L1" "U2"])
+  (wire->positions ["R75" "D30" "R83" "U83" "L12" "D49" "R71" "U7" "L72"])
+  (wire->positions ["U62" "R66" "U55" "R34" "D71" "R55" "D58" "R83"])
+  (wires->closest-intersection
+    [["R75" "D30" "R83" "U83" "L12" "D49" "R71" "U7" "L72"]
+     ["U62" "R66" "U55" "R34" "D71" "R55" "D58" "R83"]])
+  (wires->closest-intersection
+    [["R98" "U47" "R26" "D63" "R33" "U87" "L62" "D20" "R33" "U53" "R51"]
+     ["U98" "R91" "D20" "R16" "D67" "R40" "U7" "R15" "U6" "R7"]])
+
+  (def wires
+    (map
+      #(-> %
+           (clojure.string/split ","))
+      (clojure.string/split-lines
+        (fs/readFileSync "./wires.txt"))))
+  (wires->closest-intersection
+    wires)
+
+  )
+
+
+(defn main [& cli-args]
+  (prn "hello world")
+  (prn
+    (wires->closest-intersection
+      (map
+        #(-> %
+             (clojure.string/split ","))
+        (clojure.string/split-lines
+          (fs/readFileSync "./wires.txt"))))))
--- src/test/advent2019/core_test.cljs
+++ src/test/advent2019/core_test.cljs
@@ -15,3 +15,11 @@
   (is (= [2 3 0 6 99] (core/run-program [2 3 0 3 99])))
   (is (= [2 4 4 5 99 9801] (core/run-program [2 4 4 5 99 0])))
   (is (= [30 1 1 4 2 5 6 0 99] (core/run-program [1 1 1 4 99 5 6 0 99]))))
+
+(deftest wires-test
+  (is (= 610 (core/wires->closest-intersection
+               [["R75" "D30" "R83" "U83" "L12" "D49" "R71" "U7" "L72"]
+                ["U62" "R66" "U55" "R34" "D71" "R55" "D58" "R83"]])))
+  (is (= 410 (core/wires->closest-intersection
+               [["R98" "U47" "R26" "D63" "R33" "U87" "L62" "D20" "R33" "U53" "R51"]
+                ["U98" "R91" "D20" "R16" "D67" "R40" "U7" "R15" "U6" "R7"]]))))
